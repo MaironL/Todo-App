9c799bc10bf24949249e98f8fd305147d3d70218
MaironL <Mairon_488@outlook.com>
2022-02-10 16:57:08 -0400

    Todo App setup

---
+++ src/infrastructure/screens/Home/Home.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Componente Homes</h1>
+      <div className='attribution'>
+        Challenge by{' '}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href='#'>Your Name Here</a>.
+      </div>
+    </div>
+  );
+};
+
+export default Home;
